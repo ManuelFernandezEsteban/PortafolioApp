8499badc7d7edc8ed8fca0b1a005616d74fa997d
Manuel1977 <Manuel_fe1977@hotmail.com>
2021-11-04 00:17:16 +0100

    interfaces

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -22,6 +23,7 @@ import { ProductComponent } from './pages/product/product.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,11 @@
+export interface InfoPagina{
+    titulo?:string;
+    email?:string;
+    nombreCorto?:string;
+    faceBook?:string;
+    equipoTrabajo?:any[];
+    paginaAutor?:string;
+    instagram?:string;
+    twitter:string;
+    tublr?:string;
+}
